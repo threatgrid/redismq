b9b5fa69df6860fa5c3ace33bfcd62be888bda4e
Stephen Sloan <steve@stevesloan.com>
2017-10-18 10:26:23 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/redismq "0.1.0-SNAPSHOT"
+(defproject threatgrid/redismq "0.1.0"
   :description "Redis-based message queue"
   :url "http://github.com/threatgrid/redismq"
   :license {:name "Eclipse Public License"
