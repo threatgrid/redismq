6096f48b15a351449b893814c7a3290eed441b24
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2019-06-11 15:43:49 +0200

    Bump to 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/redismq "0.1.1"
+(defproject threatgrid/redismq "0.1.2-SNAPSHOT"
   :description "Redis-based message queue"
   :url "http://github.com/threatgrid/redismq"
   :license {:name "Eclipse Public License"
